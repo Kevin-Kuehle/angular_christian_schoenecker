7d9ce8c110927c03d77d55e482a61995e5d17c55
Kevin K <kevin.kuehle@googlemail.com>
2020-02-12 16:40:04 +0100

    add SiteWidth Controller

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public isMobile = false;
+  public siteWidth;
+  private mobileWidt = 890;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkSiteWidth();
+    this.siteWidthController();
   }
+
+
+
+  checkSiteWidth() {
+    this.siteWidth = window.innerWidth;
+
+    if (this.siteWidth < this.mobileWidt) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+
+    console.log(this.isMobile);
+  }
+
+  siteWidthController() {
+    window.addEventListener('resize', () => {
+      this.checkSiteWidth();
+
+    });
+  }
+
 
 }
