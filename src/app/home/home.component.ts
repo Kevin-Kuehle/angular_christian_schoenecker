8162c3177f37c53dd434743a43c1e02d941e00ce
Kevin K <kevin.kuehle@googlemail.com>
2020-02-13 15:05:30 +0100

    home site opening is responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // 'sm': em(640px, $bp-context),
+  // 'md': em(752px, $bp-context),
+  // 'lg': em(992px, $bp-context),
+  // 'xl': em(1168px, $bp-context),
+  // 'xxl': em(1366px, $bp-context));
+
+
+  public triggerWidth = 752;
+  public windowWidth;
+  public isTriggered;
+
   constructor() { }
 
   ngOnInit() {
+    this.setWidth();
+    this.windowWidthWatcher();
   }
 
+  private setWidth() {
+    this.windowWidth = window.innerWidth;
+
+    if (this.windowWidth < this.triggerWidth) {
+      this.isTriggered = true;
+    } else {
+      this.isTriggered = false;
+    }
+  }
+
+  private windowWidthWatcher() {
+    window.addEventListener('resize', () => {
+      this.setWidth();
+    });
+  }
 }
